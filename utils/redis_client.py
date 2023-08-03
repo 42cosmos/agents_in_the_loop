40c88a012cbe51e96691df5cf75fb6e6b46b9efe
Eunbin Park <parkeb417@gmail.com>
2023-08-04 00:23:23 +0900

    Add methods for find values by key pattern

--- utils/redis_client.py
+++ utils/redis_client.py
@@ -1,8 +1,16 @@
 import redis
 
+
 class RedisClient:
     def __init__(self, host='localhost', port=6379, db=0):
         self.r = redis.Redis(host=host, port=port, db=db)
+
+    def get_all_keys(self, pattern='*'):
+        return [key.decode('utf-8') for key in self.r.keys(pattern)]
+
+    def get_values_by_pattern(self, pattern):
+        keys = self.get_all_keys(pattern)
+        return {key: self.r.get(key).decode('utf-8') if self.r.get(key) else None for key in keys}
 
     def set_value(self, key, value):
         self.r.set(key, value)
@@ -12,14 +20,6 @@ class RedisClient:
         if value:
             return value.decode('utf-8')
         return None
-
-    def get_all_keys(self):
-        """
-        Get all keys from redis
-        Beware of using this function in production...
-        :return:
-        """
-        return [key.decode('utf-8') for key in self.r.keys()]
 
     def add_to_set(self, key, *values):
         self.r.sadd(key, *values)
