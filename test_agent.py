28f88a42dad14cd77ff0025b32172974e0914205
Eunbin Park <parkeb417@gmail.com>
2023-08-09 21:19:15 +0900

    Modify load to safe_load

--- test_agent.py
+++ test_agent.py
@@ -5,7 +5,7 @@ from utils.llm.agent import Student
 
 if __name__ == "__main__":
     with open("./config.yaml", "r") as f:
-        config = EasyDict(yaml.load(f, Loader=yaml.FullLoader))
+        config = EasyDict(yaml.safe_load(f))
     student = Student(config=config)
     question = "Solve the equation x^2 - 3x - 4 = 0."
     answer = student.talk_to_agent(question)
