b4dfac80f8f9cff7cda0d8aedbc4288add047c76
Eunbin Park <parkeb417@gmail.com>
2023-08-10 18:01:34 +0900

    Rename var name of redis connection

--- utils/database/redis_client.py
+++ utils/database/redis_client.py
@@ -3,35 +3,35 @@ import redis
 
 class RedisClient:
     def __init__(self, host='localhost', port=6379, db=0):
-        self.r = redis.Redis(host=host, port=port, db=db)
+        self.redis_conn = redis.Redis(host=host, port=port, db=db)
 
     def get_all_keys(self, pattern='*'):
-        return [key.decode('utf-8') for key in self.r.keys(pattern)]
+        return [key.decode('utf-8') for key in self.redis_conn.keys(pattern)]
 
     def get_values_by_key_pattern(self, pattern):
         keys = self.get_all_keys(pattern)
-        return {key: self.r.get(key).decode('utf-8') if self.r.get(key) else None for key in keys}
+        return {key: self.redis_conn.get(key).decode('utf-8') if self.redis_conn.get(key) else None for key in keys}
 
     def set_value(self, key, value):
-        self.r.set(key, value)
+        self.redis_conn.set(key, value)
 
     def get_value(self, key):
-        value = self.r.get(key)
+        value = self.redis_conn.get(key)
         if value:
             return value.decode('utf-8')
         return None
 
     def add_to_set(self, key, *values):
-        self.r.sadd(key, *values)
+        self.redis_conn.sadd(key, *values)
 
     def get_all_from_set(self, key):
-        return [value.decode('utf-8') for value in self.r.smembers(key)]
+        return [value.decode('utf-8') for value in self.redis_conn.smembers(key)]
 
     def push_to_list(self, key, *values):
-        self.r.lpush(key, *values)
+        self.redis_conn.lpush(key, *values)
 
     def get_all_from_list(self, key):
-        return [value.decode('utf-8') for value in self.r.lrange(key, 0, -1)]
+        return [value.decode('utf-8') for value in self.redis_conn.lrange(key, 0, -1)]
 
     def delete_key(self, key):
         return self.r.delete(key)
