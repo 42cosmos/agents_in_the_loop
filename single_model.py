3d06840b92db0bb14ff246f4f1d10f6c7df0455a
Eunbin Park <parkeb417@gmail.com>
2023-09-12 02:45:45 +0900

    Add get_dataset_with_train_dataset_in_arch - 실험에 사용된 학습데이터셋만 모아 일반 모델 학습 및 평가

--- single_model.py
+++ single_model.py
@@ -38,58 +38,93 @@ def validate_dataset_language(args):
             raise ValueError(error_msg.format(language=LANGUAGES[args.dataset_lang]))
 
 
+def find_logging_file_name(args):
+    model_names_for_log_name = args["model_names"].replace('-', '_')
+    model_names_for_log_name = model_names_for_log_name.replace(" ", "-")
+    logging_file_name = f"models-{model_names_for_log_name}"
+    logging_file_name += f"-dataset_{args['dataset_name']}_{args['dataset_lang']}"
+    logging_file_name += f"-data_mode_{args['mix_dataset_mode']}"
+    if args['mix_dataset_mode'] == "unlabelled":
+        logging_file_name += f"-portion_{args['portion']}"
+    logging_file_name += f"-samples_{args['n_samples']}"
+    logging_file_name += f"-sequence_length_{args['max_seq_length']}"
+    logging_file_name += f"-ask_oracle_{args['ask_oracle']}"
+    return logging_file_name
+
+
+def find_cached_train_dataset_path(args, standard_model_name_alias):
+    import os
+    dir_name = find_logging_file_name(args)
+    train_dataset_output_dir = args["output_dir"]
+    train_dataset_output_path = os.path.join(train_dataset_output_dir,
+                                             dir_name,
+                                             standard_model_name_alias,
+                                             "train_dataset")
+    if os.path.isfile(train_dataset_output_path):
+        return train_dataset_output_path
+    else:
+        raise ValueError(f"train_dataset_output_path: {train_dataset_output_path} is not exist")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_name", type=str, default="docent",
+    parser.add_argument("--dataset_name", type=str, default="bionlp2004",
                         choices=["wikiann", "polyglot", "mit_restaurant", "mit_movie_trivia", "bionlp2004", "docent"])
-    parser.add_argument("--dataset_lang", type=str, default="ko", choices=["ko", "en", "ja", "pl", "id"])
-    parser.add_argument("--model_name", type=str, default="xlm-roberta-base")  # bert-base-multilingual-uncased
-    parser.add_argument("--mix_dataset_mode", type=str, default="original", choices=["original", "unlabelled"])
+    parser.add_argument("--dataset_lang", type=str, default="en", choices=["ko", "en", "ja", "pl", "id"])
+    parser.add_argument("--model_names", type=str, default="bert-base-multilingual-uncased xlm-roberta-base")
+    parser.add_argument("--standard_model", type=str, default="bert-base-multilingual-uncased")
+    parser.add_argument("--mix_dataset_mode", type=str, default="unlabelled", choices=["original", "unlabelled"])
     parser.add_argument("--portion", type=float, default=1.0)
+    parser.add_argument("--ask_oracle", action="store_false")
+    parser.add_argument("--use_train_dataset_in_arch", action="store_false")
     args = parser.parse_args()
+    validate_dataset_language(args)  # 도메인 데이터셋과 데이터셋 언어 설정이 맞는지 확인
 
-    # 도메인 데이터셋과 데이터셋 언어 설정이 맞는지 확인
-    validate_dataset_language(args)
+    # python script.py --ask_oracle: 출력은 True
+    # python script.py: 출력은 False
+    # pycharm 실행 시 store_true -> False
+    # pycharm 실행 시 store_false -> True
 
     start_time = datetime.datetime.now()
 
-    selected_dataset = args.dataset_name
-
-    model_args = ModelArguments(model_name_or_path=args.model_name)
-    data_args = DataTrainingArguments(dataset_name=selected_dataset,
+    model_args = ModelArguments(model_name_or_path=args.standard_model)
+    data_args = DataTrainingArguments(dataset_name=args.dataset_name,
                                       dataset_lang=args.dataset_lang,
                                       portion=args.portion,
-                                      data_mode=args.mix_dataset_mode
+                                      data_mode=args.mix_dataset_mode,
+                                      make_data_pool=False
                                       )
 
-    logging_file_name = f"dataset_{args.dataset_name}-{args.dataset_lang}"
-    logging_file_name += f"-data_mode_{args.mix_dataset_mode}"
+    logging_file_name = f"dataset-{args.dataset_name}_{args.dataset_lang}"
+    logging_file_name += f"&data_mode-{args.mix_dataset_mode}"
     if args.mix_dataset_mode == "unlabelled":
-        logging_file_name += f"-portion_{args.portion}"
-    logging_file_name += f"-sequence_length_{data_args.max_seq_length}"
-    model_names_setting_for_log_name = args.model_name.replace('-', '_')
-    logging_file_name += f"-single_{model_names_setting_for_log_name}"
+        logging_file_name += f"&portion-{args.portion}"
+    logging_file_name += f"&sequence_length-{data_args.max_seq_length}"
+
+    if args.use_train_dataset_in_arch:
+        logging_file_name += "&use_train_dataset_in_arch"
+    else:
+        logging_file_name += f"&single_train"
+    model_names_setting_for_log_name = args.standard_model.replace('-', '_')
+    logging_file_name += f"-{model_names_setting_for_log_name}"
+
     setup_logging(file_name=logging_file_name)
 
     processor = NerProcessor(data_args)
 
-    init_train_dataset, pool_dataset, eval_dataset, test_dataset = processor.get_dataset()
-    if "random_ner_tags" in init_train_dataset.column_names:
-        init_train_dataset = init_train_dataset.rename_column("random_ner_tags", "ner_tags")
-
-    if "random_ner_tags" in pool_dataset.column_names:
-        pool_dataset = pool_dataset.remove_columns("ner_tags")
-        pool_dataset = pool_dataset.rename_column("random_ner_tags", "ner_tags")
-
-    init_train_dataset = init_train_dataset.cast(pool_dataset.features)
-    train_dataset = concatenate_datasets([init_train_dataset, pool_dataset])
+    train_dataset_path = None
+    if args.use_train_dataset_in_arch:
+        all_args = vars(args) | vars(model_args) | vars(data_args)
+        train_dataset_path = find_cached_train_dataset_path(all_args,
+                                                            standard_model_name_alias=args.standard_model.split("-")[0])
+    train_dataset, eval_dataset, test_dataset = processor.get_dataset(train_dataset_path=train_dataset_path)
 
     label_to_id, id_to_label = processor.label_to_id, processor.id_to_label
     label_list = processor.labels
 
     config = AutoConfig.from_pretrained(model_args.model_name_or_path, num_labels=len(label_list))
     model = AutoModelForTokenClassification.from_pretrained(model_args.model_name_or_path, config=config)
-    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, use_fast=True)  # BertTokenizerFast
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, use_fast=True)
 
     col_names = train_dataset.column_names
     encode_fn = partial(tokenize_and_align_labels,
@@ -138,7 +173,7 @@ if __name__ == "__main__":
     logging.info(f"Training results: {train_results}")
     # trainer.save_model() -> Saves the tokenizer too for easy upload
 
-    logging.info("*** Evaluate ***")
+    logging.info("***** Evaluate *****")
     metrics = trainer.evaluate()
 
     for key, value in metrics.items():
@@ -151,8 +186,15 @@ if __name__ == "__main__":
         logging.info(f"  {key} = {value}")
 
     end_time = datetime.datetime.now()
-    logging.info(
-        f"{Fore.LIGHTMAGENTA_EX}***** {args.model_name} {args.dataset_name}-{LANGUAGES[args.dataset_lang]} Final F1 score: {metrics['test_f1']}*****{Fore.RESET}")
+    if args.use_train_dataset_in_arch:
+        logging.info(
+            f"{Fore.LIGHTGREEN_EX} train_dataset: {train_dataset.num_rows:,} =>\t"
+            f"***** {args.standard_model} {args.dataset_name}-{LANGUAGES[args.dataset_lang]} "
+            f"Final F1 score: {metrics['test_f1']}*****{Fore.RESET}")
+    else:
+        logging.info(
+            f"{Fore.LIGHTMAGENTA_EX}***** {args.standard_model} {args.dataset_name}-{LANGUAGES[args.dataset_lang]} "
+            f"Final F1 score: {metrics['test_f1']}*****{Fore.RESET}")
     logging.info(
         f"Starts at {start_time.strftime('%Y-%m-%d %H-%M-%S')} -> Ends at {end_time.strftime('%Y-%m-%d %H-%M-%S')}")
     logging.info(f" 소요 시간: {end_time - start_time}")
